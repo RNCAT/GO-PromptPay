8a362f1bf0a16af2dc9ac0ac1bfb138113c527fe
RNCAT <xshootercast@gmail.com>
2019-10-31 09:49:43 +0700

    Create Repo

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"log"
+	"net/http"
+	"strconv"
+
+	pp "github.com/Frontware/promptpay"
+	"github.com/skip2/go-qrcode"
+	"html/template"
+)
+
+var tmpl = template.Must(template.ParseGlob("templates/*"))
+var fs = http.FileServer(http.Dir("static/"))
+
+func main()  {
+	log.Println("Server started at : http://localhost:8080")
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/",home)
+	http.HandleFunc("/promptpay",promtpay)
+	_ = http.ListenAndServe(":8080", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request)  {
+	_ = tmpl.ExecuteTemplate(w, "index", nil)
+}
+
+func promtpay(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST"{
+		http.Redirect(w,r,"/",301)
+	}
+	id := r.FormValue("id")
+	amount, _ := strconv.ParseFloat(r.FormValue("amount"),64)
+	payment := pp.PromptPay{
+		PromptPayID: id,
+		Amount:      amount,
+	}
+	promtPayCode, _ := payment.Gen()
+	hash := md5.New()
+	hash.Write([]byte(id))
+	filename := hex.EncodeToString(hash.Sum(nil))
+	_ = qrcode.WriteFile(promtPayCode, qrcode.Medium, 256, "./static/"+filename+".png")
+
+	_ = tmpl.ExecuteTemplate(w,"qrshow",filename)
+}
